4be6a999e408b4aa36fa25ee5c3281f50385a61b
getusol <sb23499816@163.com>
2025-09-19 23:00:00 +0800

    test1

---
+++ test1/test1.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<string>
+void circle(std::string);
+void cIrcle(void);
+std::string enter;
+int main()
+{
+	cIrcle();
+	return 0;
+}
+void circle(std::string enter)
+{
+	using namespace std;
+	if (enter == "Dian") {
+		cout << "2002\n";
+		cIrcle();
+	}
+	else if (enter != "Quit") {
+		cout << "Error!\n";
+		cIrcle();
+	}
+}
+void cIrcle()
+{
+	using namespace std;
+	cout << "Please enter: ";
+	getline(cin, enter);
+	circle(enter);
+}
